76c49b73002787a3301c2cd04cda25a73922b287
Erik Solg <solg.erik@gmail.com>
2023-12-23 05:07:46 +0700

    Intermediate changes

--- tester.py
+++ tester.py
@@ -1,32 +1,33 @@
 import pandas as pd
-from datetime import datetime, date, timedelta
+from datetime import datetime, timedelta
+import json
 
 #Constants
 HISTORY_FILE = "MES_1min_continuous_adjusted.txt"
 POSITION_FILE = "positions.csv"
 TICK_SIZE = 0.25
-STOP_LOSS = -12
-BREAK_EVEN = 5
-TAKE_PROFIT = 15
+BREAK_EVEN = 10
+TAKE_PROFIT = 20
 EXIT_PREFER = "16:00:00"
 EXIT_FINAL = "16:00:00"
-MAX_LOSS = 40
-MAX_LOSS_PER_TRADE = -12
-
-# Addition1: If we do not get 1/3 of Take profit within first hour after opening the position,
-# decrease Take profit to 2/3.
-# If we do not get 2/3 Take profit within second hour after opening the position,
-# decrease Take profit to 1/3.
-# If we do not get 1/3 take profit after third hour, and we are in profit, take profit nevertheless.
-TIME_BASED_TAKE_PROFIT = False
+MAX_LOSS = -40
+MAX_LOSS_PER_TRADE = -22
+MIN_LOSS_PER_TRADE = -1
 
 # Addition2: If we have not reached BREAK_EVEN after 30m, and we are in profit, BREAK_EVEN nevertheless
 TIME_BASED_BREAKEVEN = False
 TIME_BASED_BREAKEVEN_DURATION = timedelta(minutes=30)
 
-# If set, stop loss amount is based on losing candle low/high, not declared stop loss.
+# If set, stop loss amount is based on losing candle low/high, not declared stop loss. Simulate slippage.
 STOP_LOSS_BASED_ON_CANDLE = False
 
+TRAILING_STOP = 10 # Define the trailing stop distance
+TRAILING_STOP_ENABLED = False # Enable or disable the trailing stop
+TRIGGER_PROFIT = 15 # Define the trigger profit
+CANDLE_BASED_TRAILING_STOP_ENABLED = False
+CANDLE_BASED_TRAILING_STOP_LENGTH = 10
+
+SMART_POSITION_CLOSE = False
 
 def main():
     candleData = pd.read_csv(HISTORY_FILE)
@@ -66,7 +67,8 @@ def main():
                     positionType,
                     datetime.strptime(f"{position.date} {position.time}", "%Y-%m-%d %H:%M:%S"),
                     positionTrend,
-                    position.sl
+                    position.sl,
+                    position.tp if "tp" in position else "NA"
 
                 )
         else:
@@ -120,16 +122,18 @@ class Position:
     TREND_TRENDING = 1
     TREND_SIDEWAYS = 0
 
-    def __init__(self, positionType, timestamp, trend, stopLossPrice):
+    def __init__(self, positionType, timestamp, trend, stopLossPrice, takeProfit):
         self.positionType = positionType
         self.timestamp = timestamp
         self.status = Position.POSITION_WAITING
         self.trend = trend
         self.stopLossPrice = stopLossPrice
-        self.takeProfit = TAKE_PROFIT
+        self.highestProfit = -999
+        self.takeProfitPrice = takeProfit
         self.breakEven = BREAK_EVEN
         self.exitPrefer = EXIT_PREFER
         self.exitFinal = EXIT_FINAL
+        self.maxPL = 0
         self.strategyStages = {
             "timeBasedTakeProfit": 0,
             "timeBasedBreakEven": 0
@@ -140,6 +144,7 @@ class Position:
         self.status = Position.POSITION_OPENED
         self.entryPrice = entryPrice
         self.unrealizedPL = 0
+        self.takeProfit = abs(self.entryPrice - self.takeProfitPrice) if self.takeProfitPrice != "NA" else TAKE_PROFIT
         self.stopLoss = -1 * abs(self.stopLossPrice-entryPrice)
 
     def closePosition(self, pl = None):
@@ -148,10 +153,9 @@ class Position:
         self.status = Position.POSITION_CLOSED
         self.realizedPL = pl
         self.unrealizedPL = None
-        print(self.timestamp)
-        print(self.realizedPL)
 
     def handleUnrealizedPL(self, candle):
+
         if self.status is not Position.POSITION_OPENED:
             return
         if self.positionType == Position.POSITION_LONG:
@@ -159,16 +163,19 @@ class Position:
         else:
             self.unrealizedPL = self.entryPrice - candle.close
 
+        if self.unrealizedPL > self.highestProfit:
+            self.highestProfit = self.unrealizedPL
+            self.highestProfitLastModified = candle.timestamp
+
+
     def handleStopLoss(self, candle):
+        stopLossAmount = self.stopLoss if self.stopLoss >= MAX_LOSS_PER_TRADE else MAX_LOSS_PER_TRADE
+        stopLossAmount = stopLossAmount if stopLossAmount <= MIN_LOSS_PER_TRADE else MIN_LOSS_PER_TRADE
         if self.status is not Position.POSITION_OPENED:
             return
-        if self.positionType == Position.POSITION_LONG and self.unrealizedPL - candle.distanceToLow <= self.stopLoss:
-            stopLossAmount = self.unrealizedPL - candle.distanceToLow if STOP_LOSS_BASED_ON_CANDLE else self.stopLoss
-            stopLossAmount = stopLossAmount if stopLossAmount >= MAX_LOSS_PER_TRADE else MAX_LOSS_PER_TRADE
+        if self.positionType == Position.POSITION_LONG and self.unrealizedPL - candle.distanceToLow <= stopLossAmount:
             self.closePosition(stopLossAmount)
-        elif self.positionType == Position.POSITION_SHORT and self.unrealizedPL - candle.distanceToHigh <= self.stopLoss:
-            stopLossAmount = self.unrealizedPL - candle.distanceToHigh if STOP_LOSS_BASED_ON_CANDLE else self.stopLoss
-            stopLossAmount = stopLossAmount if stopLossAmount >= MAX_LOSS_PER_TRADE else MAX_LOSS_PER_TRADE
+        elif self.positionType == Position.POSITION_SHORT and self.unrealizedPL - candle.distanceToHigh <= stopLossAmount:
             self.closePosition(stopLossAmount)
 
 
@@ -180,51 +187,50 @@ class Position:
         elif self.positionType == Position.POSITION_SHORT and self.unrealizedPL + candle.distanceToLow >= self.takeProfit:
             self.closePosition(self.takeProfit)
 
+    def handleTrailingStop(self, candle):
+        global TRIGGER_PROFIT
+        global TRAILING_STOP
+        if self.status is not Position.POSITION_OPENED or not TRAILING_STOP_ENABLED:
+            return
 
-# Addition1:
-# 1h passed. Profit < 1/3 TP? TP to 1/3
-# 1h passed. Profit < 2/3 TP? TP to 2/3
-# 2h passed. Have not reached TP of 1/3 and in +? Close position.
-# 2h passed. Have not reached TP of 2/3 and Profit < 1/3?  TP to 1/3
-# 2h passed. Have not reached TP of 2/3,but Profit is > 1/3? SL to 1/3
-# decrease Take profit to 2/3.
-# If we do not get 2/3 Take profit within second hour after opening the position,
-# decrease Take profit to 1/3.
-# If we do not get 1/3 take profit after third hour, and we are in profit, take profit nevertheless.
-        if self.status is Position.POSITION_OPENED and TIME_BASED_TAKE_PROFIT:
-            # Takes too much time
-            if candle.timestamp - self.timestamp >= timedelta(hours=1) and self.strategyStages["timeBasedTakeProfit"] == 0:
-                self.strategyStages["timeBasedTakeProfit"] = 1
-                if self.positionType == Position.POSITION_LONG and self.unrealizedPL + candle.distanceToHigh < (self.takeProfit/3):
-                    self.takeProfit = self.takeProfit/3
-                elif self.positionType == Position.POSITION_SHORT and self.unrealizedPL + candle.distanceToLow < (self.takeProfit/3):
-                    self.takeProfit = self.takeProfit/3     
-                elif self.positionType == Position.POSITION_LONG and self.unrealizedPL + candle.distanceToHigh < (self.takeProfit/3*2):
-                    self.takeProfit = self.takeProfit/3*2
-                elif self.positionType == Position.POSITION_SHORT and self.unrealizedPL + candle.distanceToLow < (self.takeProfit/3):
-                    self.takeProfit = self.takeProfit/3*2
+        if self.positionType == Position.POSITION_LONG and self.unrealizedPL + candle.distanceToHigh >= TRIGGER_PROFIT:
+            self.stopLoss = TRIGGER_PROFIT - TRAILING_STOP
+            TRIGGER_PROFIT = TRIGGER_PROFIT + 10
 
-            elif candle.timestamp - self.timestamp >= timedelta(hours=2) and self.strategyStages["timeBasedTakeProfit"] == 1:
-                self.strategyStages["timeBasedTakeProfit"] = 2
-                if self.takeProfit == TAKE_PROFIT/3 and self.unrealizedPL > 0:
-                    self.closePosition()
-                elif self.takeProfit == TAKE_PROFIT/3*2:
-                    if self.positionType == Position.POSITION_LONG and self.unrealizedPL + candle.distanceToHigh < (self.takeProfit/2):
-                        self.takeProfit = self.takeProfit/2
-                    elif self.positionType == Position.POSITION_SHORT and self.unrealizedPL + candle.distanceToLow < (self.takeProfit/2):
-                        self.takeProfit = self.takeProfit/2
-                    else:
-                        self.stopLoss = self.takeProfit/2
+        elif self.positionType == Position.POSITION_SHORT and self.unrealizedPL + candle.distanceToLow >= TRIGGER_PROFIT:
+            self.stopLoss = TRIGGER_PROFIT - TRAILING_STOP
+            TRIGGER_PROFIT = TRIGGER_PROFIT + 10
 
+
+    def handleCandleBasedTrailingStop(self, candleGroup):
+        if self.status is not Position.POSITION_OPENED or not CANDLE_BASED_TRAILING_STOP_ENABLED:
+            return
+
+        
+        if self.positionType == Position.POSITION_LONG:
+            if candleGroup[0].lowPrice - self.entryPrice < self.unrealizedPL:
+                self.stopLoss = candleGroup[0].lowPrice - self.entryPrice
+        if self.positionType == Position.POSITION_SHORT:
+            if candleGroup[0].highPrice - self.entryPrice < self.unrealizedPL:
+                self.stopLoss = candleGroup[0].highPrice - self.entryPrice
+
+    def handleSmartPositionClose(self, candle):
+        global SMART_POSITION_CLOSE
+        if self.status is not Position.POSITION_OPENED or not SMART_POSITION_CLOSE:
+            return
+
+        if candle.timestamp - self.highestProfitLastModified > timedelta(minutes = 60) and self.unrealizedPL > 0 and self.highestProfit - self.unrealizedPL < 5:
+            print("closing smart")
+            self.closePosition()
 
     def handleBreakEven(self, candle):
         if self.status is not Position.POSITION_OPENED:
             return
         if ((self.positionType == Position.POSITION_LONG and self.unrealizedPL + candle.distanceToHigh >= self.breakEven) or
-            (self.positionType == Position.POSITION_SHORT and self.unrealizedPL + candle.distanceToLow >= self.breakEven)):
+            (self.positionType == Position.POSITION_SHORT and self.unrealizedPL + candle.distanceToLow >= self.breakEven)) and self.stopLoss<0:
             self.stopLoss = 0
 
-        if self.status is Position.POSITION_OPENED and TIME_BASED_BREAKEVEN:
+        if self.status is Position.POSITION_OPENED and TIME_BASED_BREAKEVEN and self.stopLoss<0:
             if candle.timestamp - self.timestamp >= TIME_BASED_BREAKEVEN_DURATION and self.unrealizedPL >= 0:
                 self.stopLoss = 0
 
@@ -249,27 +255,28 @@ class Session:
         self.candles = candles
         self.positions = []
         self.realizedPL = 0
-        self.maxLoss = MAX_LOSS
+        self.exceedMonthlyPL = False
 
         self.openLow = candles[0].lowPrice
         self.openHigh = candles[2].highPrice
 
-    def addPosition(self, positionType, positionTimestamp, trend, positionStopLoss):
+    def addPosition(self, positionType, positionTimestamp, trend, positionStopLoss, takeProfit):
         self.positions.append(
             Position(
                 positionType,
                 positionTimestamp,
                 trend,
-                positionStopLoss
+                positionStopLoss,
+                takeProfit
             )
         )
 
     def runBackTest(self):
         for position in self.positions:
             self.__runBackTestForPosition(position)
-            self.realizedPL += position.realizedPL
-            if self.realizedPL <= self.maxLoss:
-                break
+            self.realizedPL += position.realizedPL if position.status == Position.POSITION_CLOSED else 0
+            if position.status == Position.POSITION_WAITING:
+                print("Position did not execute: ", position)
     
     def __calculateEntryPrice(self, position, candle):
         entryPercentage = position.timestamp.second / 60
@@ -285,19 +292,49 @@ class Session:
 
     def __runBackTestForPosition(self, position):
         
+        candleGroup = []
+        candleGroupIterations = 0
         for candle in self.candles:
             if position.status == Position.POSITION_WAITING:
                 if position.timestamp >= candle.timestamp and position.timestamp < candle.timestamp + timedelta(minutes=1):
                     entryPrice = self.__calculateEntryPrice(position, candle)
-                    position.openPosition(entryPrice)
+                    position.openPosition(entryPrice)                
 
-            elif position.status == Position.POSITION_OPENED:
+            if position.status == Position.POSITION_OPENED:
+                candleGroup.append(candle)
 
                 position.handleStopLoss(candle)
                 position.handleTakeProfit(candle)
                 position.handleBreakEven(candle)
                 position.handleEndOfDay(candle)
                 position.handleUnrealizedPL(candle)
+                position.handleTrailingStop(candle)
+                position.handleSmartPositionClose(candle)
+
+                if len(candleGroup) == CANDLE_BASED_TRAILING_STOP_LENGTH:
+                    candleGroupIterations += 1
+                    position.handleCandleBasedTrailingStop(candleGroup)
+                    previousCandleGroup = candleGroup
+                    candleGroup = []
+
+                # if len(candleGroup) == CANDLE_BASED_TRAILING_STOP_LENGTH/2 and candleGroupIterations != 0:
+                #     candleGroupIterations += 1
+                #     position.handleCandleBasedTrailingStop(candleGroup)
+                #     candleGroup = candleGroup[int(CANDLE_BASED_TRAILING_STOP_LENGTH/2):]
+
+                # if position.positionType == Position.POSITION_LONG and len(candleGroup) == CANDLE_BASED_TRAILING_STOP_LENGTH/2 and candle.close < candleGroup[0].open and len(previousCandleGroup) > 0:
+                #     candleGroupIterations += 1
+                #     position.handleCandleBasedTrailingStop(previousCandleGroup[int(CANDLE_BASED_TRAILING_STOP_LENGTH/2):])
+                #     candleGroup = []
+                # if position.positionType == Position.POSITION_SHORT and len(candleGroup) == CANDLE_BASED_TRAILING_STOP_LENGTH/2 and candle.close > candleGroup[0].open and len(previousCandleGroup) > 0:
+                #     candleGroupIterations += 1
+                #     position.handleCandleBasedTrailingStop(previousCandleGroup[int(CANDLE_BASED_TRAILING_STOP_LENGTH/2):])
+                #     candleGroup = []
+                
+                # if candleGroupIterations >= 2 and len(candleGroup) == CANDLE_BASED_TRAILING_STOP_LENGTH/2:
+                #     candleGroupIterations += 1
+                #     position.handleCandleBasedTrailingStop(candleGroup)
+                #     candleGroup = []
 
     def __str__(self):
         return str(f"Session: {self.date}")
@@ -325,7 +362,7 @@ class Backtest:
             session.runBackTest()
 
     def calculateTotalPL():
-        total_PL = sum(session.realizedPL for session in Backtest.sessions)
+        total_PL = sum(session.realizedPL for session in Backtest.sessions if session.exceedMonthlyPL == False)
         return total_PL
 
     def calculateMonthlyPL():
@@ -334,27 +371,34 @@ class Backtest:
             month_year = session.date.strftime('%Y-%m') # Format the date as 'YYYY-MM'
             if month_year not in monthly_PL:
                 monthly_PL[month_year] = 0
+            if monthly_PL[month_year] < MAX_LOSS:
+                session.exceedMonthlyPL == True
+                continue
             monthly_PL[month_year] += session.realizedPL
         return monthly_PL
 
     def calculateSessionPL():
-        session_PL = {session.date: session.realizedPL for session in Backtest.sessions if len(session.positions) > 0}
+        session_PL = {session.date.strftime('%Y-%m-%d'): session.realizedPL for session in Backtest.sessions if len(session.positions) > 0}
         return session_PL
 
+    def calculateWinRatio():
+        no_sessions = len(Backtest.sessions)
+        win_sessions = len([session for session in Backtest.sessions if session.realizedPL > 0])
+        return win_sessions/no_sessions
+
     def printResults():
-        global STOP_LOSS
         global TAKE_PROFIT
         global BREAK_EVEN
         global EXIT_PREFER
 
-        print(f"Stop Loss: {STOP_LOSS}")
         print(f"Take Profit: {TAKE_PROFIT}")
         print(f"Break Even: {BREAK_EVEN}")
         print(f"Exit: {EXIT_PREFER}")
         print("")
+        print("Monthly PL: ", json.dumps(Backtest.calculateMonthlyPL()))
+        print("Session PL: ", json.dumps(Backtest.calculateSessionPL()))
         print("Total PL: ", Backtest.calculateTotalPL())
-        print("Monthly PL: ", Backtest.calculateMonthlyPL())
-        print("Session PL: ", Backtest.calculateSessionPL())
+        print("Win ratio: ", Backtest.calculateWinRatio())
 
     def writeResultsToCSV(self, filename):
         results = pd.DataFrame({
